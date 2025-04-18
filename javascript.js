234ca1cbdb2fb59af5c402a50d83ceabd30e047e
Thunder1966 <dhanushjagdish123@gmail.com>
2025-04-18 22:42:54 +0530

    Updating Assignment

---
+++ javascript.js
@@ -0,0 +1,21 @@
+var age =10
+var age =12
+let gender ='male'
+const institute ='X-workz'
+const college ="JNNCE"
+const place="shimoga"
+document.writeln(age)
+
+let emp1 ={id:12,employeename:'aa',salary:100}
+let emp2 = new Object();
+emp2.id=13;
+emp2.employeename='jdsjsd';
+emp2.salary=50898;
+emp2.id=14;
+function emp3(id,employeename,salary){
+    this.id=id;
+    this.employeename=employeename;
+    this.salary=salary;
+    
+}
+
